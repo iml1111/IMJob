4f2e34f0e663cd7de87f2340380d2115bbb8cb67
IML <shin10256@gmail.com>
2023-02-19 22:38:41 +0900

    [feat] string param converter 구현

--- IMJob/controller/job/util.py
+++ IMJob/controller/job/util.py
@@ -13,3 +13,30 @@ class Timer:
 
     def __exit__(self, *args):
         self.elapsed += (time.time() - self.start)
+
+
+def convert_param(param: str, annotation):
+    if annotation is int:
+        try:
+            return int(param), True
+        except ValueError:
+            return param, False
+
+    elif annotation is float:
+        try:
+            return float(param), True
+        except ValueError:
+            return param, False
+
+    elif (
+        annotation is bool and
+        param.lower() in ('true', 'false')
+    ):
+        return param.lower() == 'true', True
+
+    return param, False
+
+
+if __name__ == '__main__':
+    param, ok = convert_param('1', int)
+    print(param, ok)
